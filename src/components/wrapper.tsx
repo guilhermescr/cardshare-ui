4284079bdabb0dec0a1b41a6c8f32adbb38f9c91
guilhermescr <guilhermechaves2005@hotmail.com>
2025-09-07 18:29:44 -0300

    feat: add reusable wrapper component

---
+++ src/components/wrapper.tsx
@@ -0,0 +1,14 @@
+import { cn } from '@/lib/utils';
+
+interface WrapperProps {
+  className?: string;
+  children: React.ReactNode;
+}
+
+export default function Wrapper({ className, children }: WrapperProps) {
+  return (
+    <div className={cn('max-w-[1500px] mx-auto w-full px-4', className)}>
+      {children}
+    </div>
+  );
+}
