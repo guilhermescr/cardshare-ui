aa1ad3e434afd2c820a572e3d4d5988c79d4a8c3
guilhermescr <guilhermechaves2005@hotmail.com>
2025-09-07 20:34:28 -0300

    style: add wrapper to the main content of the app

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 import Header from '@/components/header';
+import Wrapper from '@/components/wrapper';
 
 interface AppLayoutProps {
   children: ReactNode;
@@ -9,7 +10,9 @@ export default function AppLayout({ children }: AppLayoutProps) {
   return (
     <div className="flex flex-col min-h-screen bg-gray-50">
       <Header />
-      <main className="flex-1 p-8">{children}</main>
+      <Wrapper>
+        <main className="flex-1 py-8">{children}</main>
+      </Wrapper>
     </div>
   );
 }
